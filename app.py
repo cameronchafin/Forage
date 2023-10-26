c31e43f193323b30366351e766d91e115ea3b5aa
cameronchafin <cameronchafin.esl@gmail.com>
2023-10-26 19:21:35 +0900

    Working Results
    Implemented backend logic for working results. Need to format description field on results page.

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request, flash, redirect
-from werkzeug.utils import secure_filename
 import datetime
 import requests
+import base64
 import os
+from werkzeug.utils import secure_filename
+
 
 API_URL = "https://mushroom.kindwise.com/api/v1/identification"
 API_KEY = "lLPXIjX93GaPI6BgVBVgFjmgSFCJp3K6B6rcXKNgvWkFgUmhXE"
+API_URL_WITH_DETAILS = f"{API_URL}?details=common_names,url,description,edibility,image"
 
 app = Flask(__name__)
+app.secret_key = "secret_key"
 year = datetime.date.today().year
 
 UPLOAD_FOLDER = 'uploads'
@@ -38,68 +42,54 @@ def contact():
 @app.route("/identify", methods=["GET", "POST"])
 def identify():
     if request.method == "POST":
-        # Check if an image was uploaded
-        if 'image' not in request.files:
-            flash('No image part')
-            return redirect(request.url)
-        file = request.files['image']
 
-        # Check if the image is valid
+        # Check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            print("File not found in request")
+            return redirect(request.url)
+        file = request.files['file']
+
+        # If the user does not select a file, the browser submits an empty file without a filename.
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
-        if file and allowed_file(file.filename):
+
+        if file:
             filename = secure_filename(file.filename)
-            image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(image_path)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
 
-            # Call your API function
-            result = identify_mushroom_with_api(image_path)
+            # Encode image to base64
+            with open(filepath, "rb") as img_file:
+                img_base64 = base64.b64encode(img_file.read()).decode("ascii")
 
-            # Depending on your results page design, you might directly render the results page here
-            return render_template('result.html', result=result)
+            print("Preparing to make the API call...")
 
-            # Or you might want to redirect to a separate results page with some session data
-            # session['result'] = result
-            # return redirect(url_for('results'))
+            response = requests.post(
+                API_URL_WITH_DETAILS,
+                json={
+                    "images": [img_base64],
+                    "similar_images": True,
+                },
+                headers={
+                    "Content-Type": "application/json",
+                    "Api-Key": API_KEY,
+                }
+            ).json()
 
-    # If it's a GET request or if there's any other condition you've not met, render the identify page.
+            # You can add more processing of the response if needed
+            suggestions = response.get("result", {}).get("classification", {}).get("suggestions", [])
+
+            # Pass suggestions to the result template
+            return render_template('result.html', year=year, suggestions=suggestions)
+
     return render_template('identify.html', year=year)
 
 
 @app.route("/result")
 def result():
     return render_template('result.html', year=year)
-
-
-def identify_mushroom_with_api(image_path):
-    headers = {
-        "Authorization": f"Bearer {API_KEY}",
-        "Api-Key": API_KEY,
-        "Content-Type": "multipart/form-data"
-    }
-
-    # Additional params
-    params = {
-        "details": "common_names,url,description,edibility,image",
-        "language": "en",
-        "async": "false"  # We want to wait for the response
-    }
-
-    with open(image_path, 'rb') as image_file:
-        files = {'image': image_file}
-        response = requests.post(API_URL + "/identification", headers=headers, files=files, params=params)
-
-    print(response.status_code)
-    print(response.text)
-
-    # Assuming the API returns data in JSON format
-    try:
-        data = response.json()
-        return data
-    except Exception as e:
-        print(f"Failed to parse JSON: {e}")
-        return None
 
 
 if __name__ == "__main__":
