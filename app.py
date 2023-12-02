ac8e0ae13f1d2f0bcc37a8507375aa08e9d1bbb8
cameronchafin <cameronchafin.esl@gmail.com>
2023-12-02 14:05:49 +0900

    Index Features
    Added features section to index page and slightly reformatted login and registration pages.
    Added scroll to top button to index page.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, redirect
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 import datetime
 import requests
 import base64
